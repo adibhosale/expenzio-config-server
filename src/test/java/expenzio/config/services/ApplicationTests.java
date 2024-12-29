06926bd93b54f3dc0b9e32e842a5c469c935ef91
Aditya Bhosale <devadviam@gmail.com>
2024-12-29 12:38:25 +0530

    bas files for expenzio config server

---
+++ src/test/java/expenzio/config/services/ApplicationTests.java
@@ -0,0 +1,13 @@
+package expenzio.config.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
